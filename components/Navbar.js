57be92a37f3cc2ea2dfb1ee2494c6566969a30ae
0xRKX <developerweb3@proton.me>
2023-10-05 22:54:27 +0200

    Components and Pages

---
+++ components/Navbar.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+import { Bakbak_One } from 'next/font/google'
+
+const inter = Bakbak_One({ subsets: ['latin'], weight: '400' })
+
+function Navbar() {
+    return (
+        <nav className={`${inter.className} text-gray-950 text-2xl font-medium z-20 relative py-12 px-16 lg:px-48 `}>
+            <ul className='flex gap-12'>
+                <Link href={'/'}><li>Home</li></Link>
+                <Link href={'/contact'}><li>Contact</li></Link>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
