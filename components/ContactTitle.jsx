a83620fe07b3bc3ab75bd9ba2084ecc954d226f1
0xRKX <developerweb3@proton.me>
2023-10-05 23:29:57 +0200

    Optimization

--- components/ContactTitle.jsx
+++ components/ContactTitle.jsx
@@ -1,6 +1,6 @@
 function ContactTitle() {
     return (
-        <div className="my-80 p-1">
+        <div className="my-72 p-1">
             <h1 className="text-6xl text-right lg:text-right lg:text-8xl">
                 Let&apos;s talk
             </h1>
