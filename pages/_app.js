39f42babaa1aa3f8b54a73e6a00944b035cf0587
0xRKX <developerweb3@proton.me>
2023-10-05 22:55:33 +0200

    Framer Motion Config

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '@/styles/globals.css'
+import { Bakbak_One } from "next/font/google"
+import Navbar from '@/components/Navbar';
+import { AnimatePresence } from 'framer-motion';
+
+const bakbak_one = Bakbak_One({ subsets: ['latin'], weight: '400' })
+
+export default function App({ Component, pageProps, router }) {
+  return (
+
+    <div className={`${bakbak_one.className}`} >
+      <Navbar />
+      <AnimatePresence >
+        <Component key={router.pathname} {...pageProps} />
+      </AnimatePresence>
+    </div >
+  )
 }
